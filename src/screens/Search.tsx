659d74abd6beb2d8a1c572fc36a9aa39d97e9a40
Youssef Bahlaoui <youssef-bahlaoui@outlook.fr>
2022-03-10 00:00:05 +0100

    handle user not found and isSubmiting status

--- src/screens/Search.tsx
+++ src/screens/Search.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Text, TextInput, View } from "react-native";
+import { Alert, Text, TextInput, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Button from "../components/Button";
 import styles from "../styles/search.style";
@@ -7,31 +7,42 @@ import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../App";
 import { findUserByLogin } from "../api/fortyTwoApi";
 import axios from "axios";
+import useAuth from "../hooks/useAuth";
 
 type SearchProps = NativeStackScreenProps<RootStackParamList, "Search">;
 
 export default function Search(props: SearchProps) {
-  const { navigation, route } = props;
+  const { navigation } = props;
+  const auth = useAuth();
   const [login, setLogin] = useState("");
+  const [isSubmiting, setIsSubmiting] = useState<boolean>(false);
 
   async function onPressSearchHandler() {
     try {
-      const user = await findUserByLogin(login, route.params.accessToken);
-      navigation.navigate("DisplayUser", {
-        user,
-      });
+      if (auth.isAuth) {
+        setIsSubmiting(true);
+        const user = await findUserByLogin(login, auth.accessToken);
+        setIsSubmiting(false);
+        navigation.navigate("DisplayUser", {
+          user,
+        });
+      }
     } catch (error: unknown) {
       if (axios.isAxiosError(error)) {
-        console.log(error.response?.status);
-        console.log(JSON.stringify(error.response?.data, null, 2));
+        const status = error.response?.status;
+        if (status) {
+          return Alert.alert("Info", `User '${login}' not found`);
+        }
       } else if (error instanceof Error) {
         console.log(error.message);
       }
+      Alert.alert("Error", "Something went wrong");
+      setIsSubmiting(false);
     }
   }
 
   function onTextChangeHandler(text: string) {
-    setLogin(text);
+    setLogin(text.trim());
   }
 
   return (
@@ -41,10 +52,14 @@ export default function Search(props: SearchProps) {
         <TextInput
           style={styles.input}
           value={login}
+          maxLength={8}
           onChangeText={onTextChangeHandler}
         />
       </View>
-      <Button containerStyle={styles.button} onPress={onPressSearchHandler}>
+      <Button
+        containerStyle={styles.button}
+        onPress={onPressSearchHandler}
+        disabled={isSubmiting}>
         Search
       </Button>
     </SafeAreaView>
